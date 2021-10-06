81f40601de1b20a2ed6bd14474f3aa9cfa5ce3c4
kvarel <61622068+kvarel@users.noreply.github.com>
2021-10-06 17:20:19 +0300

    Update Практика_по_requests.py

--- Практика_по_requests.py
+++ Практика_по_requests.py
@@ -1,4 +1,27 @@
 def calc_age(id):
+	''' если id является username, преобразуем username в id, иначе собираем данные с сайта, парсим ДР
+	добавляем в словарь и сортируем'''
 
+		
+	ACCESS_TOKEN = '17da724517da724517da72458517b8abce117da17da72454d235c274f1a2be5f45ee711'
+	URL1 = 'https://api.vk.com/method/users.get'
+	URL2 = 'https://api.vk.com/method/friends.get'
+	params = {'v': 5.71, 'fields': 'bdate', 'user_id': id}
+	data = {}
+	rez = {}
+	data = filter(data, bdate not is None)
+	for i in data:
+		if 2021 - i[bdate] in rez:
+			rez[2021 - i[bdate]] += 1
+		else:
+			rez[2021 - i[bdate]] = 1
+	rez = list(rez)
+			  
+def sort_age(lst):
+	return sorted(lst, key=lambda x: (-x[1], x[0]))
+
+def test():
+	print(sort_age([(1,2), (2,3), (5,1), (4, 1)]))
+	
 if __name__ == '__main__':
-	calc_age(input())
+    test()
