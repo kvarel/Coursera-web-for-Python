cfe245af53328108cc41ecf146a2f31c9b61e862
kvarel <61622068+kvarel@users.noreply.github.com>
2021-10-08 10:54:59 +0300

    Update Практика_по_requests.py

--- Практика_по_requests.py
+++ Практика_по_requests.py
@@ -1,36 +1,30 @@
+# взвращает отсортированный список [возраст, кол-во друзей такого возраста] из вконтакте. Отсеиваются такие, у кого не указана дата рождения или не указан год
+import requests
 def calc_age(id):
-	''' если id является username, преобразуем username в id, иначе собираем данные с сайта, парсим ДР
-	добавляем в словарь и сортируем'''
+    data = {}
+    if not id.isdigit():
+        id = search_id(id)
+    URL = 'https://api.vk.com/method/friends.get'
+    params = {'user_id': id, 'fields': 'bdate', 'v': 5.81, "access_token": ACCESS_TOKEN}
+    rez = requests.get(URL, params).json()
+    for i in rez['response']['items']:
+        if 'bdate' in i and len(i['bdate']) > 5:
+            if 2021 - int(i['bdate'][-4:]) in data:
+                data[2021 - (int(i['bdate'][-4:]))] += 1
+            else:
+                data[2021 - (int(i['bdate'][-4:]))] = 1
+    return sort_age(list(data.items()))
 
-		
-	ACCESS_TOKEN = '17da724517da724517da72458517b8abce117da17da72454d235c274f1a2be5f45ee711'
-	URL1 = 'https://api.vk.com/method/users.get'
-	URL2 = 'https://api.vk.com/method/friends.get'
-	params = {'v': 5.71, 'fields': 'bdate', 'user_id': id}
-	data = {}
-	rez = {}
-	data = filter(data, bdate not is None)
-	for i in data:
-		if 2021 - i[bdate] in rez:
-			rez[2021 - i[bdate]] += 1
-		else:
-			rez[2021 - i[bdate]] = 1
-	rez = list(rez)
-	
-def name_to_id(id):
-	import requests
-	ACCESS_TOKEN = '17da724517da724517da72458517b8abce117da17da72454d235c274f1a2be5f45ee711'
-	URL1 = 'https://api.vk.com/method/users.get'
-	params = {'v': 5.71, 'fields': 'bdate', 'user_id': id}
-	x = requests.get(URL1, params=params)
-	print(x.text)
-	
-	
+def search_id(id):
+    URL = 'https://api.vk.com/method/users.get'
+    params = {'v': 5.89, 'user_ids': id, "access_token": ACCESS_TOKEN}
+    x = requests.get(URL, params=params).json()
+    return x['response'][0]['id']
+
 def sort_age(lst):
-	return sorted(lst, key=lambda x: (-x[1], x[0]))
+    return sorted(lst, key=lambda x: (-x[1], x[0]))
 
-def test():
-	print(sort_age([(1,2), (2,3), (5,1), (4, 1)]))
-	
 if __name__ == '__main__':
-    name_to_id('kvarel')
+    ACCESS_TOKEN = '14a8293414a8293414a829343c14d1b46d114a814a8293475f7c1dfbf7f186e38dc7912'
+    res = calc_age(input())
+    print(res)
